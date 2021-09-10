534a2f7d84ddb32d8fe3752e606cce6eca53d84f
kkt12121 <kkt34343@gmail.com>
2021-09-11 00:42:40 +0900

    Add_magazine_api_crud

--- src/modules/magazines/magazines.controller.ts
+++ src/modules/magazines/magazines.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Param,
+  Get,
+  Post,
+  Put,
+  Delete,
+  UseGuards,
+  UseInterceptors,
+  ParseIntPipe,
+} from "@nestjs/common";
 import { CurrentUser, ReqUser } from "../auth/auth.decorator";
 import { CreateMagazineDto } from "./magazines.dto";
 import { MagazinesService } from "./magazines.service";
@@ -7,4 +19,48 @@ import { JwtAuthGuard } from "../auth/auth.guard";
 @Controller("magazines")
 export class MagazinesController {
   constructor(private readonly magazineService: MagazinesService) {}
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Post("create")
+  @UseGuards(JwtAuthGuard)
+  public async create(
+    @Body() createMagazineDto: CreateMagazineDto,
+    @ReqUser() { id: userId }: CurrentUser
+  ) {
+    return this.magazineService.create(createMagazineDto, userId);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get(":id")
+  public async findOne(@Param("id", ParseIntPipe) id: number) {
+    return this.magazineService.findOne({ id });
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get()
+  public async findAll() {
+    const { items } = await this.magazineService.findAll();
+    return items;
+  }
+
+  @Put(":id/update")
+  @UseGuards(JwtAuthGuard)
+  public async update(
+    @Body() magazine: any,
+    @Param("id", ParseIntPipe) id: number,
+    @ReqUser() { id: userId }: CurrentUser
+  ) {
+    await this.magazineService.update(magazine, id, userId);
+    return "매거진 수정 완료!";
+  }
+
+  @Delete(":id/delete")
+  @UseGuards(JwtAuthGuard)
+  public async delete(
+    @Param("id", ParseIntPipe) id: number,
+    @ReqUser() { id: userId }: CurrentUser
+  ) {
+    await this.magazineService.delete(id, userId);
+    return "매거진 삭제 완료!";
+  }
 }
--- src/modules/magazines/magazines.service.ts
+++ src/modules/magazines/magazines.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import {
+  Injectable,
+  ForbiddenException,
+  NotFoundException,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { Magazine } from "./magazine.entity";
 import {
   MagazineFindAllOptions,
@@ -10,4 +15,52 @@ import { CreateMagazineDto } from "./magazines.dto";
 @Injectable()
 export class MagazinesService {
   constructor(private readonly magazineRepository: MagazineRepository) {}
+
+  public async create(createMagazineDto: CreateMagazineDto, userId: number) {
+    const { photo, title, body } = createMagazineDto;
+
+    if (title.length > 30)
+      throw new ForbiddenException("제목은 30자를 넘을 수 없습니다.");
+
+    const magazine = new Magazine({ photo, title, body, userId });
+    await this.magazineRepository.save(magazine);
+    return this.magazineRepository.findOne({ id: magazine.id });
+  }
+
+  public async findOne(options?: MagazineFindOneOptions) {
+    const magazine = await this.magazineRepository.findOne(options);
+    if (!magazine) throw new NotFoundException("매거진이 없습니다");
+    return magazine;
+  }
+
+  public async findAll(options?: MagazineFindAllOptions) {
+    return this.magazineRepository.findAll(options);
+  }
+
+  public async update(magazine: any, magazineId: number, userId: number) {
+    const findMagazine = await this.magazineRepository.findOne({
+      id: magazineId,
+    });
+
+    if (!findMagazine) throw new NotFoundException("해당 매거진이 없습니다");
+
+    if (findMagazine.userId !== userId)
+      throw new UnauthorizedException("권한이 없습니다");
+
+    await this.magazineRepository.update(magazineId, magazine);
+  }
+
+  public async delete(magazineId: number, userId: number) {
+    console.log(magazineId, userId);
+    const findMagazine = await this.magazineRepository.findOne({
+      id: magazineId,
+    });
+
+    if (!findMagazine) throw new NotFoundException("해당 매거진이 없습니다");
+
+    if (findMagazine.userId !== userId)
+      throw new UnauthorizedException("권한이 없습니다");
+
+    await this.magazineRepository.delete(magazineId);
+  }
 }
